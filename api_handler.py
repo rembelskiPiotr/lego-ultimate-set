029a72568475d3ecf1d55e3e702ff8e76c4c4436
Piotr Rembelski <piotr.rembelski@shijigroup.com>
2022-09-06 23:03:47 +0200

    handling-api-response-paging

---
+++ api_handler.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+
+
+def build_url(endpoint, API_KEY, page_size):
+    base_url = 'https://rebrickable.com/api/v3/lego/'
+    url = base_url + endpoint + '/?key=' + API_KEY + '&page_size=' + page_size
+    return url
+
+
+def get_df_from_api(endpoint, API_KEY, page_size):
+    url = build_url(endpoint, API_KEY, page_size)
+    response = requests.get(url).json()
+    df = pd.json_normalize(response, 'results')
+    next_url = response['next']
+    print(next_url)
+    while next_url:
+        response = requests.get(next_url).json()
+        df = pd.concat([df, pd.json_normalize(response, 'results')])
+        next_url = response['next']
+        print(next_url)
+    return df
