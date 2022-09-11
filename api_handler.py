70eed53e6043b38085a06e1f96fff7491d069d39
Piotr Rembelski <piotr.rembelski@shijigroup.com>
2022-09-11 17:31:08 +0200

    adding-1-sec-sleep-to-avoid-throttling

--- api_handler.py
+++ api_handler.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import time
 
 
 def build_url(endpoint, API_KEY, page_size):
@@ -13,10 +14,9 @@ def get_df_from_api(endpoint, API_KEY, page_size):
     response = requests.get(url).json()
     df = pd.json_normalize(response, 'results')
     next_url = response['next']
-    print(next_url)
     while next_url:
         response = requests.get(next_url).json()
         df = pd.concat([df, pd.json_normalize(response, 'results')])
         next_url = response['next']
-        print(next_url)
+        time.sleep(1)
     return df
